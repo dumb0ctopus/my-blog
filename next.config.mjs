5064f7606a0b52800a2b5524e8b75f91e77004e9
Jésùloluwa <jesuloluwap@gmail.com>
2024-11-01 12:08:24 +0100

    .next.config.js

--- next.config.mjs
+++ next.config.mjs
@@ -1,23 +1,7 @@
-// next.config.js
-const withBundleAnalyzer = require("@next/bundle-analyzer")({
-  enabled: process.env.ANALYZE === "true",
-});
-
 /** @type {import('next').NextConfig} */
-module.exports = withBundleAnalyzer({
-  output: "standalone", // Streamline deployment for serverless functions
-  swcMinify: true, // Use SWC minification for smaller bundles
-  webpack(config, { isServer }) {
-    config.externals = config.externals || {};
+const nextConfig = {
+  output: "standalone",
+  swcMinify: true,
+};
 
-    // Example of excluding a large package (replace with specific packages if needed)
-    config.externals["@next/env"] = "commonjs @next/env";
-
-    // If you have other large packages that are not needed server-side, exclude them here
-    if (isServer) {
-      config.externals.push("@some-large-package"); // Replace with actual large package names, if any
-    }
-
-    return config;
-  },
-});
+export default nextConfig;
