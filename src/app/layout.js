def0f0200426423859bc74afafed094b95f1f5da
Jésùloluwa <jesuloluwap@gmail.com>
2024-11-20 12:18:38 +0100

    og-image

--- src/app/layout.js
+++ src/app/layout.js
@@ -2,6 +2,7 @@ import "./globals.css";
 import Header from "@/components/Header/Header";
 import Footer from "@/components/Footer/Footer";
 import { Inter, Merriweather } from "@next/font/google";
+import siteMetadata from "@/utils/siteMetadata";
 
 const inter = Inter({
   weight: ["400", "700"],
@@ -16,6 +17,25 @@ const merriweather = Merriweather({
 export const metadata = {
   title: "Jesuloluwa",
   description: "My Lazy Notes",
+  openGraph: {
+    title: "Jesuloluwa",
+    description: "My Lazy Notes",
+    url: siteMetadata.siteUrl,
+    images: [
+      {
+        url: `${siteMetadata.siteUrl}/images/og-image.png`,
+        alt: "Jesuloluwa's Blog",
+      },
+    ],
+    siteName: "Jesuloluwa's Blog",
+    type: "website",
+  },
+  twitter: {
+    card: "summary_large_image",
+    title: "Jesuloluwa",
+    description: "My Lazy Notes",
+    images: [`${siteMetadata.siteUrl}/images/og-image.png`],
+  },
 };
 
 export default function RootLayout({ children }) {
